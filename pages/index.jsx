66f45996d5e4430b73c982cbfb2d67d9700d4069
Mutesi <ktesialine@gmail.com>
2022-11-23 10:16:36 +0200

    feat: create tag component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,15 @@
+import Tag from "../components/ui/Tag";
+
 export default function Home() {
   return (
-    <h1 className="text-4xl  text-center">
-      <span className="text-red-500"> RED!</span> coming soon
-    </h1>
+    <>
+      <h1 className="text-4xl  text-center">
+        <span className="text-red-500"> RED!</span> coming soon
+      </h1>
+      <div className="w-fit m-auto">
+        <Tag>02 Oct 2022</Tag>
+        <Tag fill>DAOs</Tag>
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,26 @@ module.exports = {
       fontFamily:{
          sans:["Neufile Grotesk"] // called it sans for it to act as default
       },
+      letterSpacing:{
+        tight:"-1%" // this a letter spacing -1 percentage
+      },
+      lineHeight:{
+        18:"1.125rem" // this is 18px for line height
+      },
       colors:{
         purple:'#5602E0', //this is Purple
         'grey':{
           DEFAULT:'#9A9A9A', //this is dark grey
           50:'#E4E4E4', //this is light grey
-          100:'#878787' //this is  grey for the tags
+          100:'#878787', //this is  grey for the tags text
+          200:'#F4F4F4'   // this is tag background color
         }
+      },
+      spacing:{
+        0.6:"0.1875rem" // this is 3px
+      },
+      borderRadius:{
+        md:"1.1875rem"// this is 19px for border radius
       }
     },
   },
