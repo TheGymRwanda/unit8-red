06fcc9d6a57c9d65d5a0ddfb22265f3a2e7b49fb
hirwaaldo1 <hirwaaldo1@gmail.com>
2022-11-24 16:29:57 +0200

    feat: add expertise section with it's responsiveness

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
+import Expertise from "../components/sections/homepage/Expertise";
 import Hero from "../components/sections/homepage/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
+      <Hero />
+      <Expertise />
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,23 @@ module.exports = {
       },
       spacing: {
         0.6: "0.1875rem", // this is 3px
-        4.5:'1.125rem',
-        6.75:'1.6875rem' , // this is 27px
-        25.25:'6.3125rem', // 101px
-        5.30: '1.3256rem',  //21.21px
-        4.48: '1.1213rem', //17.94px
-        8.25: '2.0625rem', // 33px
-        16.69: '4.1744rem', //66px
-        26:'6.5rem', //104px
-        34:'8.5rem', //136px
+        4.5: "1.125rem",
+        6.75: "1.6875rem", // this is 27px
+        25.25: "6.3125rem", // 101px
+        5.3: "1.3256rem", //21.21px
+        4.48: "1.1213rem", //17.94px
+        8.25: "2.0625rem", // 33px
+        16.69: "4.1744rem", //66px
+        26: "6.5rem", //104px
+        34: "8.5rem", //136px
+        21.25: "5.3125rem", //85px
+        20.25: "5.0625rem", //81px
+        1.75: "0.4375rem", //7px
+        6.23: "1.5594rem", //24.91px
+        26.45: "6.6125rem", //105.8px
+        33: "8.25rem", //132px
+        22.45: "5.6125rem", //89.8px
+        35.55: "8.8875rem", //142.2px
       },
       borderRadius: {
         md: "1.1875rem", // this is 19px for border radius
@@ -36,6 +44,7 @@ module.exports = {
           50: "#E4E4E4", //this is light grey
           100: "#878787", //this is  grey for the tags
           200: "#F4F4F4", // this is tag background color
+          300: "#E2E2E2", // this is color of border
         },
       },
       lineHeight: {
@@ -55,8 +64,12 @@ module.exports = {
       },
       maxWidth: {
         "8xl": "82rem", //1312px
-        '4xl': '50.375rem',  // 806px
-        '3xl':'47.3125rem'  // 757px
+        "4xl": "50.375rem", // 806px
+        "3xl": "47.3125rem", // 757px
+      },
+      width: {
+        64.37: "16.0938rem", //257.5px,
+        151.25: "37.8125rem", //605px
       },
       fontSize: {
         "6xl": "3.5rem", // this is the size of 56px
