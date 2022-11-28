2a66b4a8b05fad538682e9a3d7db06aa3a773136
izzett222 <iizzeddin62@gmail.com>
2022-11-28 15:01:33 +0200

    feat: blog cards container overflow on the right

--- components/sections/homepage/Blog.jsx
+++ components/sections/homepage/Blog.jsx
@@ -6,7 +6,7 @@ import data from '../../../data/blogCard'
 import BlogCard from "../../ui/BlogCard";
 export default function Blog() { 
   return (
-    <section className="mt-27.79 mb-31.75">
+    <section className="mt-27.79 mb-31.75 overflow-hidden">
       <Wrapper>
         <div className="flex gap-4.25 mb-5 md:mb-8">
           <h3>Blog</h3>
@@ -15,7 +15,7 @@ export default function Blog() {
             <Image src={rightArrow} alt="" />
           </div>
         </div>
-        <div className="flex overflow-hidden gap-5 md:gap-6">
+        <div className="flex gap-5 md:gap-6">
           {data.map(el => <BlogCard key={el.id} date={el.date} title={el.title} subtitle={el.subtitle} tags={el.tag} />)}
 
         </div>
