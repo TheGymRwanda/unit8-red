4b50e4847be663831bd33358c728c76797ca034a
Mutesi <ktesialine@gmail.com>
2022-11-29 09:23:42 +0200

    feat: install swiper package and add some configuration

--- components/sections/homepage/Blog.jsx
+++ components/sections/homepage/Blog.jsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Wrapper from "../../layout/Wrapper";
 import rightArrow from "/public/assets/icons/blue-right-arrow.svg";
 import leftArrow from "/public/assets/icons/gray-left-arrow.svg";
-import data from '../../../data/blogCard'
+import data from "../../../data/blogCard";
 import BlogCard from "../../ui/BlogCard";
-export default function Blog() { 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+export default function Blog() {
   return (
     <section className="mt-27.79 mb-31.75 overflow-hidden">
       <Wrapper>
@@ -15,8 +17,25 @@ export default function Blog() {
             <Image src={rightArrow} alt="" />
           </div>
         </div>
-        <div className="flex gap-5 md:gap-6">
-          {data.map(el => <BlogCard key={el.id} date={el.date} title={el.title} subtitle={el.subtitle} tags={el.tag} />)}
+        <div className="flex gap-5 md:gap-6 w-full ">
+          <Swiper
+            spaceBetween={200}
+            slidesPerView={3}
+            onSlideChange={(e) => console.log(e.clickedIndex)}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            {data.map((el) => (
+              <SwiperSlide key={el.id}>
+                <BlogCard
+                  key={el.id}
+                  date={el.date}
+                  title={el.title}
+                  subtitle={el.subtitle}
+                  tags={el.tag}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </Wrapper>
     </section>
