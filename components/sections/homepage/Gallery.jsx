042d99e95ce67c8f571b56ba9a1070f7c00bda7a
hirwaaldo1 <hirwaaldo1@gmail.com>
2022-12-01 15:14:05 +0200

    feat: add react konva in gallery section

--- components/sections/homepage/Gallery.jsx
+++ components/sections/homepage/Gallery.jsx
@@ -1,80 +1,175 @@
-import { useRef, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import { useRef, useState, useEffect } from "react";
 import Wrapper from "../../layout/Wrapper";
-import Image from "next/image";
+import { Stage, Layer, Image, Rect } from "react-konva";
 
 export default function Gallery() {
-  let [prev, setPrev] = useState(null);
-  let [url, setUrl] = useState(0);
-  let [zIndex, setZIndex] = useState(0);
-  let container = useRef()
-  const imgArr = ["/assets/images/gallery-1.webp","/assets/images/gallery-2.webp","/assets/images/gallery-3.webp",]
-  function handleEvent(event) {
-    const info = container.current.getBoundingClientRect();
-    const firstY = event.clientY - info.top;
-    const firstX = event.clientX - info.left;
+  let [coods, setCoods] = useState([]);
+  let images = [useRef(), useRef(), useRef()];
+  let [dimension, setDimension] = useState(null);
+  let [mobileDimension, setMobileDimension] = useState(null);
+  let container = useRef();
+  let mobileContainer = useRef();
+  useEffect(() => {
+    setDimension(container.current.clientWidth)
+    setMobileDimension(mobileContainer.current.clientWidth)
 
-    if (!prev) {
-      const img = document.createElement("img");
-      container.current.appendChild(img);
-      img.src = imgArr[url];
-      if (url === imgArr.length - 1) {
-        setUrl(0)
-      } else {
-        setUrl(url + 1)
+  }, [])
+  // const handleMouse = useCallback(
+  const handleMouse = (event) => {
+    const randomNumber = Math.floor(Math.random() * images.length)
+    if (coods.length < 1) {
+      let x = event.evt.offsetX -
+        images[randomNumber].current.clientWidth / 2
+      let y = event.evt.offsetY -
+        images[randomNumber].current.clientHeight / 2;
+      if (x < 0) {
+        x = 2
       }
-      img.className = "gallery-img"
-      setPrev([firstX, firstY]);
-      
-      img.style.top = (firstY - (img.clientHeight /2)) + "px";
-      img.style.left = (firstX - (img.clientWidth /2)) + "px";
-      img.alt=zIndex
-      img.style.zIndex = zIndex;
-      console.log(event.clientY - info.top, event.clientX - info.left);
-      
+      else if (x + images[randomNumber].current.clientWidth >= event.currentTarget.attrs.container.clientWidth) {
+        x = event.currentTarget.attrs.container.clientWidth - images[randomNumber].current.clientWidth;
+      }
+      if (y < 0) {
+        y = 0;
+      } else if (y + images[randomNumber].current.clientHeight >= event.currentTarget.attrs.container.clientHeight) {
+        y = event.currentTarget.attrs.container.clientHeight - images[randomNumber].current.clientHeight;
+      }
+      setCoods([
+        ...coods,
+        {
+          x,
+          y,
+          img: images[randomNumber],
+          w: images[randomNumber].current.clientWidth,
+          h: images[randomNumber].current.clientHeight,
+          first: true,
+        },
+      ]);
     } else {
-      if (Math.abs(firstX - prev[0]) > 62 || Math.abs(firstY - prev[1]) > 62) {
-        let img = document.createElement("img");
-        img.className = "gallery-img"
-        img.src = imgArr[url];
-        if (url === imgArr.length - 1) {
-            setUrl(0)
-          } else {
-            setUrl(url + 1)
-          }
-        console.log("all time", firstX - prev[0], firstY - prev[1]);
-        container.current.appendChild(img);
-        img.style.top = (firstY - (img.clientHeight /2)) + "px";
-        img.style.left = (firstX - (img.clientWidth /2)) + "px";
-        img.alt=zIndex
-        setZIndex(zIndex + 1);
-        img.style.zIndex = zIndex;
-        console.log(event.clientY - info.top, event.clientX - info.left);
-        const prevW = img.previousElementSibling.clientWidth;
-        const prevH = img.previousElementSibling.clientHeight;
-        const div = document.createElement("div");
-        img.previousElementSibling.replaceWith(div);
-        div.className = "gallery-img"
-        div.style.backgroundColor = "#E4E4E4"
-        div.style.height = prevH + "px";
-        div.style.width = prevW + "px";
-        div.style.top = (prev[1]-(prevH/2))  + "px"
-        div.style.left = (prev[0]-(prevW/2)) + "px"
-        setPrev([firstX, firstY]);
+      if (
+        Math.abs(
+          event.evt.offsetX -
+          (images[randomNumber].current.clientWidth / 2) -
+          coods[coods.length - 1].x
+        ) > 100 ||
+        Math.abs(
+          event.evt.offsetY -
+          images[randomNumber].current.clientHeight / 2 -
+          coods[coods.length - 1].y
+        ) > 100
+      ) {
+        let x = event.evt.offsetX -
+          images[randomNumber].current.clientWidth / 2
+        let y = event.evt.offsetY -
+          images[randomNumber].current.clientHeight / 2;
+        if (x < 0) {
+          x = 2
+        } else if (x + images[randomNumber].current.clientWidth >= event.currentTarget.attrs.container.clientWidth) {
+          x = event.currentTarget.attrs.container.clientWidth - images[randomNumber].current.clientWidth;
+        }
+        if (y < 0) {
+          y = 0;
+        } else if (y + images[randomNumber].current.clientHeight >= event.currentTarget.attrs.container.clientHeight) {
+          y = event.currentTarget.attrs.container.clientHeight - images[randomNumber].current.clientHeight;
+        }
+        setCoods([
+          ...coods.slice(0, coods.length - 1),
+          { ...coods[coods.length - 1], first: false, img: null },
+          {
+            x,
+            y,
+            img: images[randomNumber],
+            w: images[randomNumber].current.clientWidth,
+            h: images[randomNumber].current.clientHeight,
+            first: true,
+          },
+        ]);
       }
     }
   }
   return (
     <Wrapper>
-      <div
-        className="h-214.19 flex items-center justify-center relative  overflow-hidden"
-        onMouseMove={handleEvent}
-        ref={container}
-      >
-        <p>Tap anywhere to see more of us</p>
+      <div ref={container} className="hidden lg:block">
+        {!!dimension && (
+          <Stage width={dimension} height={856} onMouseMove={handleMouse}>
+            <Layer>
+              {coods.map((el, index) => {
+                return el.first ? (
+                  <Image
+                    key={index}
+                    image={el.img.current}
+                    alt=" "
+                    x={el.x}
+                    y={el.y}
+                    width={el.w}
+                    height={el.h}
+                  />
+                ) : (
+                  <Rect
+                    key={index}
+                    fill="#E4E4E4"
+                    x={el.x}
+                    y={el.y}
+                    width={el.w}
+                    height={el.h}
+                  />
+                );
+              })}
+            </Layer>
+          </Stage>
+        )}
       </div>
-      <Image src="/assets/images/gallery-1.webp" className="hidden" alt="" fill />
-      <Image src="/assets/images/gallery-2.webp" className="hidden" alt="" fill />
-      <Image src="/assets/images/gallery-3.webp" className="hidden" alt="" fill />
+      <div ref={mobileContainer} className="block lg:hidden">
+        {!!mobileDimension && (
+          <Stage width={mobileDimension} height={856} onClick={handleMouse}>
+            <Layer>
+              {coods.map((el, index) => {
+                return el.first ? (
+                  <Image
+                    key={index}
+                    image={el.img.current}
+                    alt=" "
+                    x={el.x}
+                    y={el.y}
+                    width={el.w}
+                    height={el.h}
+                  />
+                ) : (
+                  <Rect
+                    key={index}
+                    fill="#E4E4E4"
+                    x={el.x}
+                    y={el.y}
+                    width={el.w}
+                    height={el.h}
+                  />
+                );
+              })}
+            </Layer>
+          </Stage>
+        )}
+      </div>
+
+      <div className="h-0 overflow-hidden">
+        <img
+          ref={images[0]}
+          className="max-h-72"
+          src="/assets/images/gallery-1.webp"
+          alt=""
+        />
+        <img
+          src="/assets/images/gallery-2.webp"
+          className="max-h-72"
+          ref={images[1]}
+          alt=""
+        />
+        <img
+          src="/assets/images/gallery-3.webp"
+          className="max-h-72"
+          ref={images[2]}
+          alt=""
+        />
+      </div>
     </Wrapper>
   );
 }
