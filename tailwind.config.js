f03f1e9e0a3d6556e4708ad4efc8798e5a77ae91
Mutesi <ktesialine@gmail.com>
2022-12-06 17:22:54 +0200

    fix: change the width  of expertise section on desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -122,6 +122,7 @@ module.exports = {
       width: {
         64.37: "16.0938rem", //257.5px,
         151.25: "37.8125rem", //605px
+        40.37:"40.375rem",  //646px
       },
       fontSize: {
         "6xl": "3.5rem", // this is the size of 56px
