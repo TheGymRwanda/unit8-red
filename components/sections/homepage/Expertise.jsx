fecd90ea579c58806a075ecdd5d794c5e0b9bf80
rithaven <veneruth@gmail.com>
2022-12-06 19:09:26 +0200

    refactor: expertise data

--- components/sections/homepage/Expertise.jsx
+++ components/sections/homepage/Expertise.jsx
@@ -1,5 +1,5 @@
 import Wrapper from "../../layout/Wrapper";
-
+import data from "../../../data/expertiseList";
 export default function Expertise() {
   return (
     <Wrapper>
@@ -7,11 +7,9 @@ export default function Expertise() {
         <h3 className="pb-1.75 md:pb-6">Expertise</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6.23 xl:gap-x-6">
           <div className="space-y-2 md:space-y-3">
-            <p className="border-b border-grey-300">Product Development</p>
-            <p className="border-b border-grey-300">Community management</p>
-            <p className="border-b border-grey-300">Governance</p>
-            <p className="border-b border-grey-300">Product Development</p>
-            <p className="border-b border-grey-300">Product Development</p>
+          {data.map((value)=>(
+            <p className="border-b border-grey-300">{value}</p>
+          ))}
           </div>
           <div className="space-y-2 md:space-y-3 pt-3 md:pt-0">
             <p className="border-b border-grey-300">Product Design</p>
---
+++ data/expertiseList.js
@@ -0,0 +1,17 @@
+const data =[
+        "Product Development",
+        "Community management",
+        "Governance",
+        "Product Development",
+   
+    // {
+    //     "Product Development",
+    //     "Product Design",
+    //     "Strategy",
+    //     "Product Design",
+    //     "Product Design",
+    //     "Product Design",
+    // }
+        
+]
+export default data;
