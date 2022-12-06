a96be3d8c9f712cae4f44b8523a856f8020e6f2c
mhenriette <mhenriette00@gnmail.com>
2022-12-06 18:12:19 +0200

    fix: removed underline and arrow effects on mobile

--- components/ui/ProjectCard.jsx
+++ components/ui/ProjectCard.jsx
@@ -3,7 +3,7 @@ export default function ProjectCard({ src, text, id, show }) {
   return (
     <div
       className={` xl:mt-0 xl:absolute w-full duration-300 ease-out  transition-all ${
-        id === show ? "mt-5.1575 md:mt-4.75 lg:mt-2.9 mb-21.6575 md:mb-15.01 lg:mb-15.15 xl:mb-0 opacity-100 h-auto" : "opacity-0 h-0 overflow-hidden"
+        id === show ? "mt-5.1575 md:mt-4.75 lg:mt-2.9 xl:pt-4.5 mb-21.6575 md:mb-15.01 lg:mb-15.15 xl:mb-0 opacity-100 h-auto" : "opacity-0 h-0 overflow-hidden"
       }`}
     >
       <div className="relative w-full xl:h-111 drop-shadow-sm rounded-xl">
--- components/ui/ProjectTitle.jsx
+++ components/ui/ProjectTitle.jsx
@@ -8,7 +8,7 @@ export default function ProjectTitle({ href, children, handle, remove, mobile, s
       {!mobile && <Link
         onMouseOver={handle}
         onMouseLeave={remove}
-        className="z-10 hover:after:bg-black flex items-start gap-2 group after:transition-colors after:duration-300  after:block after:absolute relative after:bottom-0.6 after:w-full after:h-0.5 after:bg-transparent   "
+        className="z-10 hover:after:bg-black flex items-start gap-2 group after:hidden after:xl:block after:transition-colors after:duration-300 after:absolute relative after:bottom-0.6 after:w-full after:h-0.5 after:bg-transparent   "
         href={href}
       >
         <h1>{children}</h1>
@@ -18,12 +18,12 @@ export default function ProjectTitle({ href, children, handle, remove, mobile, s
           priority
           width="14"
           height="13"
-          className="pt-3 transition-opacity duration-300 inline-block opacity-0 group-hover:opacity-100"
+          className="hidden xl:inline-block pt-4 transition-opacity duration-300 opacity-0 group-hover:opacity-100"
         />
       </Link>}
       {mobile && <span
         onClick={handle}
-        className="z-10 cursor-pointer hover:after:bg-black flex items-start gap-2 group after:transition-colors after:duration-300  after:block after:absolute relative after:bottom-0.6 after:w-full after:h-0.5 after:bg-transparent   "
+        className="z-10 cursor-pointer hover:after:bg-black flex items-start gap-2 group after:transition-colors after:duration-300 after:hidden after:xl:block after:absolute relative after:bottom-0.6 after:w-full after:h-0.5 after:bg-transparent"
       >
         <h1>{children}</h1>
         <Image
@@ -32,7 +32,7 @@ export default function ProjectTitle({ href, children, handle, remove, mobile, s
           priority
           width="14"
           height="13"
-          className="pt-3 transition-opacity duration-300 inline-block opacity-0 group-hover:opacity-100"
+          className="pt-3 transition-opacity duration-300 opacity-0 group-hover:opacity-100 hidden xl:inline-block"
         />
       </span>}
     </>
