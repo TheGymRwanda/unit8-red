71f00415e39f191c9bd5c99e2fd23892c19b7554
Ntwali-Josue <kntwalijos@gmail.com>
2022-12-12 21:11:33 +0200

    Refactor:  project title details array

---
+++ data/projectTitles.js
@@ -0,0 +1,39 @@
+const projectTitles = [
+    {
+        index: 1, 
+        link: "https://dacade.org/",
+        name: "Dacade",
+    },
+    {
+        index: 2, 
+        link: "https://www.bitlipa.com/",
+        name: "Bitlipa",
+    },
+    {
+        index: 3, 
+        link: "https://www.symplifi.co/",
+        name: "Symplifi",
+    },
+    {
+        index: 4, 
+        link: "https://dacade.org/",
+        name: "UTU.io",
+    },
+    {
+        index: 5, 
+        link: "https://neueux.com/apps",
+        name: "NeueUX",
+    },
+    {
+        index: 6, 
+        link: "/",
+        name: "Lab3",
+    },
+    {
+        index: 7, 
+        link: "https://evenmusic.co/",
+        name: "Even",
+    },
+]
+
+export default projectTitles;
