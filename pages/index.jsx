c16193f0de9e1f1250519e4c04aa2a961c7e30c1
Mutesi <ktesialine@gmail.com>
2022-12-15 13:17:52 +0200

    feat: add favicon and text is highlighted when use select it

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import Expertise from "../components/sections/homepage/Expertise";
 import Footer from "../components/layout/Footer";
 import Hero from "../components/sections/homepage/Hero";
 import Blog from "../components/sections/homepage/Blog";
+import Head from "next/head";
 // import Gallery from "../components/sections/homepage/Gallery";
 import dynamic from "next/dynamic";
 
@@ -13,14 +14,19 @@ const Gallery = dynamic(
 );
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <Projects />
-      <Expertise />
-      <About />
-      <Gallery />
-      <Blog />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Ape-Unit</title>
+      </Head>
+      <main>
+        <Hero />
+        <Projects />
+        <Expertise />
+        <About />
+        <Gallery />
+        <Blog />
+        <Footer />
+      </main>
+    </>
   );
 }
